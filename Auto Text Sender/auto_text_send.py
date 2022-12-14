dafa4ca2e27c292986eb7f52821a3916add16246
CL <75270770+ongcl03@users.noreply.github.com>
2022-12-15 02:22:40 +0800

    Update auto_text_send.py

--- Auto Text Sender/auto_text_send.py
+++ Auto Text Sender/auto_text_send.py
@@ -1,1 +1,18 @@
+import pyautogui, time
 
+
+text = input("Enter the text you want to send : ")
+loop_number = int(input("Enter how many times you want to send the text : "))
+time_to_wait = int(input("Enter time to wait to send the text in seconds : "))
+
+time.sleep(time_to_wait)
+
+
+for i in range(loop_number):
+    
+    try:
+        pyautogui.typewrite(text)
+        pyautogui.press("Enter")
+
+    except Exception:
+        pass
